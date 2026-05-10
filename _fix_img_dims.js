ffa8402fde98ab3d16eab02a6242f2f8d8c74cd8
Bestcreeper <152311617+Bestcreeper@users.noreply.github.com>
2026-05-10 22:25:27 +0200

    Delete _fix_img_dims.js

--- _fix_img_dims.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const f = 'C:\\Users\\ROG\\.qclaw\\workspace-agent-d2068023\\projects\\gambonanza-guide\\themes\\gambonanza-theme\\layouts\\index.html';
-let c = fs.readFileSync(f, 'utf8');
-c = c.replace(/width="831" height="128"/g, 'width="414" height="160"');
-fs.writeFileSync(f, c, 'utf8');
-console.log('img dims updated');
